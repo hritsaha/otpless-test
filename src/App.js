1b535cc80ffaa05f1e449e87f9440f4643019dc5
Hrit Saha <hritsaha2001@gmail.com>
2023-07-05 13:37:06 +0530

    second

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ function App() {
         useEffect(() => {
                 window.otpless = (otplessUser) => {
                  alert(JSON.stringify(otplessUser));
+                 console.log(JSON.stringify(otplessUser));
                 };
                }, []);
   return (
-    <div>
+    <div class="bb">
       <script type="text/javascript" src="https://otpless.com/auth.js" ></script>
+      <p id="pp">This is a test for OTP-LESS</p>
     </div>
   )
 }
