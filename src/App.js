511e58e295d3f7ee3035817b63d010cd52ffe59c
Hrit Saha <hritsaha2001@gmail.com>
2023-07-05 13:26:48 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react'
 
 function App() {
+        useEffect(() => {
+                window.otpless = (otplessUser) => {
+                 alert(JSON.stringify(otplessUser));
+                };
+               }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <script type="text/javascript" src="https://otpless.com/auth.js" ></script>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
